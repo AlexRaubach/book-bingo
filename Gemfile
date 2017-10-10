bb4325a618ac6efd29c87ec03df77d33f7632587
Antigrapist <antigrapist@gmail.com>
2017-10-10 00:03:48 -0400

    added more gems and initial work on user oauth

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'goodreads'
 
 gem 'oauth'
 
+gem "figaro"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -51,3 +53,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+    Callback_Url = "http://alexraubach.com"
+
+
+    def authenticate
+        consumer = OAuth::Consumer.new(ENV["goodreads_key"],
+                               ENV["goodreads_secret"],
+                               :site => 'https://www.goodreads.com')
+        request_token = consumer.get_request_token(:oauth_callback => Callback_Url)
+
+    end
+
+
 end
